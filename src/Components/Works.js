0b5abcfd32a4a706222f42c2c7baa347eb7eb1b3
VLadYxa343 <50592770+VLadYxa343@users.noreply.github.com>
2020-07-27 19:35:19 +0500

    Added working section

--- src/Components/Works.js
+++ src/Components/Works.js
@@ -1,8 +1,90 @@
 import React from 'react';
 
-function Works(props) {
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
     return (
-        <div></div>
+        <div
+            className={className}
+            style={{ ...style, display: "block"}}
+            onClick={onClick}
+        ><img src="https://img.icons8.com/windows/32/000000/right.png"/></div>
+    );
+}
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", left: "-35px"}}
+            onClick={onClick}
+        >
+            <img src="https://img.icons8.com/windows/32/000000/left.png"/>
+        </div>
+    );
+}
+
+function Works(props) {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+    };
+
+    const data = [
+        {
+            name: "Шкаф с АВР",
+            date: "19.09.19",
+            img: "image 1.jpg"
+        },
+        {
+            name: "Шкаф с АВР",
+            date: "19.09.19",
+            img: "image 1.jpg"
+        },
+        {
+            name: "Шкаф с АВР",
+            date: "19.09.19",
+            img: "image 1.jpg"
+        },
+        {
+            name: "Шкаф с АВР",
+            date: "19.09.19",
+            img: "image 1.jpg"
+        }
+    ]
+    const cards = data.map((el, i) => (
+        <div className="wrap" key={i}>
+            <div className="card">
+                <div className="img-wrap">
+                    <img src={require("../img/" + el.img)} alt=""/>
+                </div>
+                <div className="bottom">
+                    <div>
+                        <h3>{el.name}</h3>
+                        <p>Сделанно: {el.date}</p>
+                    </div>
+                    <a href=""><button>Оставить заявку</button></a>
+                </div>
+            </div>
+        </div>
+    ))
+    return (
+        <section className="Works">
+            <div className="container">
+                <h2>Наши работы</h2>
+                <Slider {...settings}>
+                    {cards}
+                </Slider>
+            </div>
+        </section>
     );
 }
 
